f94b40460de5df15c31d288850e6be3eef962364
Roberto A S Zanin <raszanin@gmail.com>
2022-11-29 19:32:25 -0300

    escola.js

---
+++ IntroducaoProgramacao/20221129/escola.js
@@ -0,0 +1,12 @@
+let nota1 = 7;
+let nota2 = 7;
+let nomeAluno = "Roberto";
+let faltas = 3;
+
+let media = (nota1 + nota2) / 2;
+
+if (media >= 7 && faltas <= 3) {
+  console.log(nomeAluno);
+} else {
+  console.log("Reprovado");
+}
